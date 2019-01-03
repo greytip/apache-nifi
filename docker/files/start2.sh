74d199ce72520fc9b9a2f2bb6bb74253371d2eb6
Sayeed Anjum <sayeed@greytip.com>
2019-01-03 13:21:08 +0530

    added ldap settings

--- docker/files/start2.sh
+++ docker/files/start2.sh
@@ -29,13 +29,9 @@ export NIFI_REMOTE_INPUT_HOST=$FQDN
 
 echo "Cluster address: $NIFI_CLUSTER_ADDRESS"
 
-# KEYSTORE_FOLDER=${NIFI_BASE_DIR}/nifi-certs/
-# mkdir -p ${KEYSTORE_FOLDER}
-
 #export KEYSTORE_PATH=${KEYSTORE_FOLDER}/keystore.jks
 #export TRUSTSTORE_PATH=${KEYSTORE_FOLDER}/truststore.jks
 
-# cp ${NIFI_BASE_DIR}/data/cert/*.jks $KEYSTORE_FOLDER
 
 old_keystore_pass=$(jq -r .keyStorePassword ${NIFI_BASE_DIR}/data/cert/config.json)
 old_truststore_pass=$(jq -r .trustStorePassword ${NIFI_BASE_DIR}/data/cert/config.json)
@@ -43,17 +39,28 @@ old_truststore_pass=$(jq -r .trustStorePassword ${NIFI_BASE_DIR}/data/cert/confi
 export KEYSTORE_PASSWORD=$(jq -r .keyStorePassword ${NIFI_BASE_DIR}/data/cert/config.json)
 export TRUSTSTORE_PASSWORD=$(jq -r .trustStorePassword ${NIFI_BASE_DIR}/data/cert/config.json)
 
+#export KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD2
+#export TRUSTSTORE_PASSWORD=$TRUSTSTORE_PASSWORD2
+
 export KEYSTORE_PATH=${NIFI_BASE_DIR}/data/cert/keystore.jks
 export KEYSTORE_TYPE='JKS'
 export TRUSTSTORE_PATH=${NIFI_BASE_DIR}/data/cert/truststore.jks
 export TRUSTSTORE_TYPE='JKS'
 
-echo "Changing password for stores..."
+# echo "Changing password for stores... $KEYSTORE_PASSWORD -- $KEYSTORE_PASSWORD2"
 
-#keytool -storepasswd -new $KEYSTORE_PASSWORD -keystore $KEYSTORE_PATH -storepass old_keystore_pass
-#keytool -storepasswd -new $TRUSTSTORE_PASSWORD -keystore $TRUSTSTORE_PATH -storepass old_truststore_pass
+KEYSTORE_FOLDER=${NIFI_BASE_DIR}/nifi-certs/
+if [ ! -e ${NIFI_BASE_DIR}/nifi-certs ]; then
+  mkdir -p ${KEYSTORE_FOLDER}
+  cp ${NIFI_BASE_DIR}/data/cert/*.jks $KEYSTORE_FOLDER
 
-echo "Password changed for stores..."
+  keytool -storepasswd -new $KEYSTORE_PASSWORD2 -keystore $KEYSTORE_FOLDER/keystore.jks -storepass $old_keystore_pass
+  keytool -storepasswd -new $TRUSTSTORE_PASSWORD2 -keystore $KEYSTORE_FOLDER/truststore.jks -storepass $old_truststore_pass
+else
+  echo 'Password already updated. Skipping.'
+fi
+
+# echo "Done changing passwords"
 
 exec ${scripts_dir}/start.sh
 
